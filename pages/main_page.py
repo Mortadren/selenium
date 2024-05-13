8cf9eba54027cf46d39eb0fa1e1eb98bba9794b2
Artyom Streltsov <artyom.streltsov@pin-progin.ru>
2024-05-13 15:55:03 +0700

    made edits

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,9 +2,11 @@ from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
-button_sign_in_selector = (By.CSS_SELECTOR, '[data-ga-stats-name="auth_block_login"]')
+button_sign_in_selector = (
+    By.CSS_SELECTOR, '[data-ga-stats-name="auth_block_login"]')
 icon_selector = (By.CLASS_NAME, '_1tlqpaw1 _15r2d5o1')
 # addd = (By.CLASS_NAME, 'knfad11')
+
 
 class Main_page(BasePage):
     def __init__(self, browser):
@@ -12,17 +14,13 @@ class Main_page(BasePage):
 
     def open(self):
         self.browser.get('https://www.drom.ru/')
-        
+
     def button_sign_in(self):
         return self.find(button_sign_in_selector)
-    
+
     def button_click(self):
         sign_in_button = self.button_sign_in()
         sign_in_button.click()
-    
+
     def find_icon_for_asset(self):
         return self.find(icon_selector)
-    
-    # def asdas(self):
-    #     return self.find(addd)
-    
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,21 +3,12 @@ from pages.login_page import Login_page
 from pages.main_page import Main_page
 
 
-
 def test_opening(browser):
     mane_page = Main_page(browser)
     login_page = Login_page(browser)
     mane_page.open()
-    # assert mane_page.asdas() 
     mane_page.button_click()
     login_page.insert_login()
     login_page.insert_pass()
     login_page.click_button_sign()
     assert mane_page.find_icon_for_asset()
-    
-    # assert mane_page.button_sign_in
-
-
-# def test_button_sign_in(browser):
-#     mane_page = Main_page(browser)
-#     mane_page.button_sign_in.click()
