4735c75da68389f48945d444cd30ef49caf07817
Artyom Streltsov <artyom.streltsov@pin-progin.ru>
2024-05-20 14:57:54 +0700

    add .env and replaced secret data

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,34 +1,36 @@
+import os
+from dotenv import load_dotenv
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from ui_selectors import LoginPageSelectors
 
-sign_field_selector = (By.ID, 'sign')
-password_field_selector = (By.ID, 'password')
-button_sign_selector = (By.ID, 'signbutton')
 
-LOGIN = '89994516217'
-PASSWORD = 'Artyom#Access!'
+load_dotenv()
+login = os.getenv("LOGIN")
+password = os.getenv("PASSWORD")
+
 
 class Login_page(BasePage):
     def __init__(self, browser):
         super().__init__(browser)
 
     def find_sign_field(self):
-        return self.find(sign_field_selector)
-    
+        return self.find(LoginPageSelectors.sign_field_selector)
+
     def insert_login(self):
         sign_field = self.find_sign_field()
-        sign_field.send_keys(LOGIN)
+        sign_field.send_keys(login)
 
     def find_pass_field(self):
-        return self.find(password_field_selector)
+        return self.find(LoginPageSelectors.password_field_selector)
 
     def insert_pass(self):
         pass_field = self.find_pass_field()
-        pass_field.send_keys(PASSWORD)
+        pass_field.send_keys(password)
 
     def find_button_sign(self):
-        return self.find(button_sign_selector)
-    
+        return self.find(LoginPageSelectors.button_sign_selector)
+
     def click_button_sign(self):
         button_sign = self.find_button_sign()
         button_sign.click()
